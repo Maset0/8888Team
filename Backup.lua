c36389f5585ad800d47d3a24e1ec1344bc28fbf0
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-10 12:01:14 +0200

    Update Backup.lua

--- Backup.lua
+++ Backup.lua
@@ -7,47 +7,43 @@ local Tab = Window:MakeTab({
         PremiumOnly = false
     })
     
-    local Section = Tab:AddSection({
-        Name = "Info"
-    })
-    
     Tab:AddParagraph("Made By 8888 Team","Owner Masqta_#6880")
-
-    local Section = Tab:AddSection({
+    
+     local Section = Tab:AddSection({
         Name = "Status"
     })
-
-    Tab:AddLabel("Dupe: Online")
+    
+     Tab:AddLabel("Dupe: Online")
 
     Tab:AddLabel("Trade Scam: Online")
-
-    local Section = Tab:AddSection({
+    
+     local Section = Tab:AddSection({
         Name = "Update info"
-    }
-
-    Tab:AddParagraph("[⚡ANIME] UPDATE","Version 3.7")
-
-      local Tab = Window:MakeTab({
-        Name = "Dupe Script",
+    })
+    
+     Tab:AddParagraph("[⚡ANIME] UPDATE","Version 3.7")
+     
+     local Tab = Window:MakeTab({
+        Name = "Dupe",
         Icon = "rbxassetid://4483345998",
         PremiumOnly = false
     })
-
-    local Section = Tab:AddSection({
+     
+     local Section = Tab:AddSection({
         Name = "Dupe Section"
     })
     
     Tab:AddButton({
         Name = "(RISK) Start Dupe",
         Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info1.lua"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/8888Team/main/info1.lua"))()
           end    
     })
     
     local Section = Tab:AddSection({
         Name = "Dupe Options"
     })
-
+    
     Tab:AddSlider({
         Name = "Select Pets Limits",
         Min = 0,
@@ -60,7 +56,7 @@ local Tab = Window:MakeTab({
             print(Value)
         end    
     })
-
+    
     Tab:AddToggle({
         Name = "Dupe Only Equiped Pets",
         Default = false,
@@ -68,16 +64,15 @@ local Tab = Window:MakeTab({
             print(Value)
         end    
     })
-
-    Tab:AddToggle({
-        Name = "(Risk) Dupe Gems",
+     
+     Tab:AddToggle({
+        Name = "(RISK) Dupe Gems",
         Default = false,
         Callback = function(Value)
             print(Value)
         end    
     })
-
-
+     
      Tab:AddTextbox({
         Name = "Rename all pets",
         Default = "Text Goes here",
@@ -93,12 +88,10 @@ local Tab = Window:MakeTab({
         PremiumOnly = false
     })
     
-    Tab:AddLabel("Enable Trade Scam Before Trading")
-
     local Section = Tab:AddSection({
 	Name = "ON/OFF"
     })
-
+    
     Tab:AddButton({
         Name = "ON",
         Default = false,
@@ -106,7 +99,7 @@ local Tab = Window:MakeTab({
              loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info.lua"))()
         end    
     })
-
+    
     Tab:AddButton({
         Name = "OFF",
         Default = false,
@@ -114,12 +107,12 @@ local Tab = Window:MakeTab({
             print("button pressed")
         end    
     })
-
+    
     local Section = Tab:AddSection({
-        Name = "Scam Trade Options"
+        Name = "Trade Scam Options"
     })
     
-    Tab:AddSlider({
+     Tab:AddSlider({
         Name = "Lag Victim For",
         Min = 0,
         Max = 60,
@@ -132,14 +125,14 @@ local Tab = Window:MakeTab({
         end    
     })
     
-    Tab:AddToggle({
-        Name = "Lag Victim Trade",
+    Tab:AddButton({
+        Name = "LagTrade",
         Default = false,
         Callback = function(Value)
             print(Value)
         end    
     })
-
+    
     Tab:AddParagraph("Bot-Trade","Coming Soon!")
 
       local Tab = Window:MakeTab({
@@ -147,38 +140,46 @@ local Tab = Window:MakeTab({
         Icon = "rbxassetid://4483345998",
         PremiumOnly = false
     })
-
+    
     local Section = Tab:AddSection({
         Name = "Server"
     })
-
-    Tab:AddButton({
+    
+     Tab:AddButton({
         Name = "Crash Server",
         Callback = function()
                   print("button pressed")
           end    
     })
-
+    
     Tab:AddButton({
         Name = "Hop Servers",
         Callback = function()
                   print("button pressed")
           end    
     })
-
+    
     local Section = Tab:AddSection({
         Name = "Other Scripts"
     })
-
+    
     Tab:AddButton({
         Name = "Infinite Yields",
         Callback = function()
             loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
           end    
     })
-
-    Tab:AddParagraph("8888 Team Discord","Coming Soon!)
-
-
-
-    OrionLib:Init()
+    
+       OrionLib:MakeNotification({
+        Name = "8888 Teams Welcome's You!",
+        Content = "Version 3.7 ",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+    
+    
+    
+     OrionLib:Init()
+    
+    
+    
